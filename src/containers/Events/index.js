bf5ec8e74e72f20e86996f9a5f6544591841e31b
Lylouf13 <lylian.leutard@gmail.com>
2024-03-18 08:20:38 +0100

    Removed commentary lines

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,28 +14,6 @@ const EventList = () => {
   const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
-  // Version en un seul bloc (pas de gestion du nombre de pages, donc potentiellement cassé au niveau du nombre de pages, ça doit être faisable de l'implémenter proprement ceci dit)
-
-  // ---------------------------------------------
-  // const filteredEvents = (data?.events || []).filter((event) => {
-  //   if (!type ? type === null : type === event.type){
-  //     return true;
-  //   }
-  //   return false;
-  // }).filter((_, index) => { 
-  //   if((currentPage - 1) * PER_PAGE <= index && PER_PAGE * currentPage > index){
-  //     return true
-  //   }
-  //   return false
-  // });
-  // ---------------------------------------------
-
-  //
-  // v Versions en Deux blocs v
-  // -- ça me permet d'utiliser filteredEvents indépendemment pour générer le nombre de pages nécessaires en fonction du nombre d'éléments triés
-  // -- displayedEvent récupère 9 éléments dans filteredEvents en fonction de currentPage(index = 0-8 pour currentPage =1, index = 9-17 pour currentPage=2 etc...)
-  // -- et il est utilisé après en .map pour générer les éléments
-
   const filteredEvents = (data?.events || []).filter((event) => {
     if (!type ? type === null : type === event.type){
       return true;
@@ -54,7 +32,6 @@ const EventList = () => {
     setType(evtType);
   };
 
-  // Definit la quantite de pages a generer pour la categorie
   const pageNumber = (filteredEvents?.length || 0)%PER_PAGE === 0 ? 
     Math.floor((filteredEvents?.length || 0)/ PER_PAGE) : 
     Math.floor(((filteredEvents?.length || 0)/ PER_PAGE))+1;
